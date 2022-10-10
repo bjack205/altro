04c0faf6ca98ae56d2afa6f81e15eff5c241a5b4
Brian Jackson <bjack205@gmail.com>
2022-10-10 09:42:47 -0400

    DI with second-order-cone constraint

--- test/double_integrator_test.cpp
+++ test/double_integrator_test.cpp
@@ -373,3 +373,120 @@ TEST(DoubleIntegrator, ControlBounds) {
 
   EXPECT_EQ(solver.GetIterations(), 5);
 }
+
+TEST(DoubleIntegrator, SOCControlBounds) {
+  const int num_horizon = 10;
+  const int num_states = 2 * dim;
+  const int num_inputs = dim;
+  float tf = 5.0;
+  const float h = tf / static_cast<double>(num_horizon);
+
+  // Objective
+  Eigen::VectorXd Q = Eigen::VectorXd::Constant(num_states, 1.0);
+  Eigen::VectorXd R = Eigen::VectorXd::Constant(num_inputs, 1e-2);
+  Eigen::VectorXd x0(num_states);
+  Eigen::VectorXd xf(num_states);
+  Eigen::VectorXd uf(num_inputs);
+  x0 << 2.0, 2.0, 0.0, 0.0;
+  xf.setZero();
+  uf.setZero();
+
+  // Goal Constraint
+  auto goalcon = [num_states, xf](double *c, const double *x, const double *u) {
+    int n = num_states;
+    (void)u;
+    for (int i = 0; i < n; ++i) {
+      c[i] = x[i] - xf[i];
+    }
+  };
+  auto goaljac = [num_states, num_inputs](double *jac, const double *x, const double *u) {
+    int n = num_states;
+    int m = num_inputs;
+    (void)x;
+    (void)u;
+    Eigen::Map<Eigen::MatrixXd> J(jac, n, n + m);
+    for (int i = 0; i < n; ++i) {
+      J(i, i) = 1.0;
+    }
+  };
+
+  // Control Bound Constraint
+  const a_float u_bnd = 1.0;
+  auto usoc_con = [u_bnd](double *c, const double *x, const double *u) {
+    (void)x;
+    int m = 2;
+    for (int i = 0; i < m; ++i) {
+      c[i] = u[i];
+    }
+    c[m] = u_bnd;
+  };
+  auto usoc_jac = [u_bnd](double *jac, const double *x, const double *u) {
+    (void)x;
+    (void)u;
+    int n = 4;
+    int m = 2;
+    Eigen::Map<Eigen::MatrixXd> J(jac, m + 1, 6);
+    J.setZero();
+    for (int i = 0; i < m; ++i) {
+      J(i, n + i) = 1.0;
+    }
+  };
+
+  // Define the problem
+  ErrorCodes err;
+  ALTROSolver solver(num_horizon);
+  solver.SetDimension(num_states, num_inputs, 0, LastIndex);
+  solver.SetTimeStep(h, 0, LastIndex);
+  solver.SetExplicitDynamics(dyn, jac, 0, LastIndex);
+  err = solver.SetLQRCost(num_states, num_inputs, Q.data(), R.data(), xf.data(), uf.data(), 0,
+                          LastIndex);
+  PrintErrorCode(err);
+  solver.SetInitialState(x0.data(), x0.size());
+  solver.SetConstraint(goalcon, goaljac, num_states, ConstraintType::EQUALITY, "Goal constraint",
+                       num_horizon, 0, nullptr);
+  solver.SetConstraint(usoc_con, usoc_jac, num_inputs + 1, ConstraintType::SECOND_ORDER_CONE,
+                       "Control bounds", 0, num_horizon, nullptr);
+  solver.Initialize();
+  EXPECT_TRUE(solver.IsInitialized());
+  fmt::print("Solver Initialized.\n");
+
+  // Set the initial trajectory
+  Eigen::VectorXd xinit = x0;
+  Eigen::VectorXd uinit = Eigen::VectorXd::Zero(num_inputs);
+  solver.SetState(xinit.data(), xinit.size(), 0, LastIndex);
+  solver.SetInput(uinit.data(), uinit.size(), 0, LastIndex);
+
+  // Get Initial Cost
+  double cost_initial = solver.CalcCost();
+  fmt::print("Initial cost = {}\n", cost_initial);
+
+  // Solve
+  AltroOptions opts;
+  opts.verbose = Verbosity::Inner;
+  opts.penalty_initial = 1.0;
+  opts.penalty_scaling = 100;
+  solver.SetOptions(opts);
+  SolveStatus status = solver.Solve();
+  EXPECT_EQ(status, SolveStatus::Success);
+
+  // Print last state
+  double cost_final = solver.CalcCost();
+  fmt::print("Final cost = {}\n", cost_final);
+
+  Eigen::VectorXd x_0(num_states);
+  Eigen::VectorXd x_N(num_states);
+  solver.GetState(x_0.data(), 0);
+  solver.GetState(x_N.data(), num_horizon);
+  double dist_to_goal = (x_N - xf).norm();
+  fmt::print("Distance to Goal: {}\n", dist_to_goal);
+  EXPECT_LT(dist_to_goal, 1e-4);
+
+  // Check that the controls are saturated in the norm
+  solver.PrintInputTrajectory();
+  VectorXd u(2);
+  for (int k = 0; k < 3; ++k) {
+    solver.GetInput(u.data(), 0);
+    EXPECT_NEAR(u.norm(), u_bnd, 1e-2);
+  }
+  EXPECT_EQ(solver.GetIterations(), 9);
+}
